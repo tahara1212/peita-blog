0f5cad0519f8ea3a0534f5e6a620bd2fd439e3b9
shumpei.tahara <shumpei.tahara@nijibox.co.jp>
2022-04-06 17:52:02 +0900

    mv追加

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { client } from "../libs/client";
 import Layout from "../src/components/Layout/Layout";
+import Mv from "../src/components/Mv/Mv";
 import { formatDate } from '../src/utils/date';
 
 import styles from '../styles/Home.module.scss';
@@ -9,7 +10,8 @@ export default function Home({ articles }) {
   console.log(articles);
   return (
     <Layout>
-      <ul>
+      <Mv />
+      <ul className={styles.container}>
         {articles.map((articles) => (
           <li key={articles.id} className={styles.articleList}>
             <Link href={`/articles/${articles.id}`}>
@@ -25,6 +27,11 @@ export default function Home({ articles }) {
                   </time>
                   <h2 className={styles.title}>{articles.title}</h2>
                   <p className={styles.text}>{articles.desc}</p>
+                  <div>
+                    <div className={styles.more}>
+                      <p className={styles.moreText}>more</p>
+                    </div>
+                  </div>
                 </div>
               </a>
             </Link>
---
+++ src/components/Mv/Mv.js
@@ -0,0 +1,11 @@
+import styles from './Mv.module.scss';
+
+const Mv = () => {
+  return (
+    <div className={styles.mv}>
+      <img src="/images/mv.png" className={styles.mv} />
+    </div>
+  )
+}
+
+export default Mv;
